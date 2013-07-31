19edabb5c9645ae898269f43284f98c6ea294266
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-31 18:42:06 -0500

    removed copies of extra files

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,6 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model 
 
   def name
     "#{self.first_name} #{self.last_name}"
@@ -12,11 +11,3 @@ class Student < ActiveRecord::Base
   end
  
 end
-
-
-  #rake db:create => creates database
-  #rake db:migrate => runs migrations 
-  #rake db:seed => run seed file, add shit to tables
-  #rake console => run irb, using the data in your application 
-  #rake db:drop => drops the entire the entire db
-# student = S
